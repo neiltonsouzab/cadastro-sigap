633df4df93d76af5e4bf706d961a99448f1b68e0
Neilton Barbosa <neilton.souzab@gmail.com>
2021-04-10 13:54:39 -0400

    Ultimos ajustes

--- backend/src/shared/infra/typeorm/index.ts
+++ backend/src/shared/infra/typeorm/index.ts
@@ -1,12 +1,3 @@
 import { createConnection } from 'typeorm';
 
-createConnection({
-  name: 'default',
-  type: 'mysql',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  entities: ['./src/modules/**/infra/typeorm/entities/*.ts'],
-});
+createConnection();
